d4e400454bfd0c3e3a3e342062306918ad5ba7e1
Remco <Remco.boys@gmail.com>
2017-02-08 12:08:31 +0100

    Get users from user json object

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var users = {
+  1: {
+    id: '1'
+  },
+  2: {
+    id: '2'
+  }
+};
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
@@ -11,8 +19,19 @@ app.all('*', function(req,res,next){
   next();
 });
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
+app.get('/', function (req, res) {
+  res.status(200);
+  res.send('Welcome to the express server with a sql database');
+});
+
+app.get('/users', function (req, res) {
+  res.json(users);
+});
+
+app.get('/users/:id', function(req, res) {
+  var user = users[req.params.id];
+  res.status(200);
+  res.json(user);
 });
 
 var port = process.env.PORT || 3000;
